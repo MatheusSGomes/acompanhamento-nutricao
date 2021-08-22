68e2cf9d398d6cb8c08fccdd16f20eabf6af76a5
Matheus Gomes <math.s.gomes@gmail.com>
2021-08-22 18:19:05 -0300

    novas funções criadas, codigo refatorado com funções específicas

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -13,17 +13,17 @@ for(let i = 0; i < pacientes.length; i++) {
   
   
   // VALIDAÇÃO
-  let pesoEhValido = true;
-  let alturaEhValida = true;
+  let pesoEhValido = validaPeso(peso);
+  let alturaEhValida = validaAltura(altura);
   
-  if(peso <= 0 || peso >= 1000) {
+  if(!pesoEhValido) {
     console.warn('Peso inválido!');
     pesoEhValido = false;
     tdImc.textContent = 'Peso inválido!';
     paciente.classList.add('paciente-invalido');
   }
   
-  if(altura <= 0 || altura >= 3.00) {
+  if(!alturaEhValida) {
     console.warn('Altura inválida!')
     alturaEhValida = false;
     tdImc.textContent = 'Altura inválida!';
@@ -33,6 +33,22 @@ for(let i = 0; i < pacientes.length; i++) {
   if(pesoEhValido && alturaEhValida){
     const imc = calculaImc(peso, altura)
     tdImc.textContent = imc
+  }
+}
+
+function validaPeso(peso) {
+  if(peso >= 0 && peso < 1000) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validaAltura(altura) {
+  if(altura >= 0 && altura < 3.0) {
+    return true;
+  } else {
+    return false;
   }
 }
 
--- js/form.js
+++ js/form.js
@@ -9,13 +9,36 @@ botaoAdicionar.addEventListener('click', function(event) {
   // Criar TR e TDs do paciente
   const pacienteTr = montarTr(paciente)
 
+  const erros = validaPaciente(paciente)
+  console.log(erros)
+  // Validar no clique do botão
+  if(erros.length > 0) {
+    exibirMensagensErros(erros);
+
+    return; // Sai da função sem adicionar na tabela. 
+  }
+
   const tabela = document.querySelector('#tabela-pacientes')
 
   tabela.appendChild(pacienteTr)
 
   // Limpar formulário após paciente adicionado
   form.reset()
+  const mensagensErro = document.querySelector('#mensagens-erros')
+  mensagensErro.innerHTML = ''
 })
+
+function exibirMensagensErros(erros) {
+  const ul = document.querySelector('#mensagens-erros')
+
+  ul.innerHTML = ''
+
+  erros.forEach(function(erro) {
+    const li = document.createElement('li')
+    li.textContent = erro
+    ul.appendChild(li)
+  })
+}
 
 // função recebe o formulário e retorna os dados dos pacientes.
 function obterPacienteFormulario(form) {
@@ -62,4 +85,41 @@ function montaTd(dado, classe) {
   td.classList.add(classe)
 
   return td
+}
+
+// Validação Direto no Formulário
+function validaPaciente(paciente) {
+  
+  let erros = []
+
+  // Valida se foi colocado nome no paciente
+  if(paciente.nome.length == 0) {
+    erros.push('O nome não pode ser em branco')
+  }
+
+  if(paciente.gordura.length == 0) {
+    erros.push('A gordura não pode ser em branco')
+  }
+
+  if(paciente.peso.length == 0) {
+    erros.push('O peso não pode ser em branco')
+  }
+
+  if(paciente.altura.length == 0) {
+    erros.push('A altura não pode ser em branco')
+  }
+
+  // Se o peso não for válido, é adicionado o erro no array.
+  if(!validaPeso(paciente.peso)) {
+    erros.push('O peso é inválido')
+  }
+  
+  // Se o altura não for válida, é adicionado o erro no array.
+  if(!validaAltura(paciente.altura)) {
+    erros.push('Altura Inválida')
+  }
+
+  
+
+  return erros // retorna array com ou sem erros
 }
